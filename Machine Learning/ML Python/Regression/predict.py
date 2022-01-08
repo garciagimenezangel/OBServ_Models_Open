c22dc252913f756f250b0b59b6c0b88bedc8065c
garciagimenezangel <garciagimenezangel@gmail.com>
2022-01-08 07:15:34 +0100

    Update predict.py

--- Machine Learning/ML Python/Regression/predict.py
+++ Machine Learning/ML Python/Regression/predict.py
@@ -1,7 +1,5 @@
 """
 Script to compute predictions, based on already trained models.
-Note: the script 'utils/process_GEE_features_into_predictors.py' can be used to generate the csv file that is needed
-here to compute predictions
 
 This is the last step of a process that includes the following operations:
 1) Prepare data (data_preparation.py)
@@ -29,12 +27,10 @@ def fillEmptyColumns(ref_columns, data):
     return data_filled
 
 n_features = 49
-model = pickle.load(open(root_folder + 'data/trained_models/nusvr_full.pkl', 'rb')) # full: model trained using the entire database; training: model trained using only the training subset
+model = pickle.load(open(root_folder + 'data/trained_models/nusvr_train.pkl', 'rb')) # full: model trained using the entire database; training: model trained using only the training subset
 
-# TEST 1: compute predictions from a dataset where all the predictors are present and standardized
-# NOTE: if the 'test' dataset does not have a column used as a predictor in the model (for example, it might be the case
-# of types of crop), we fill it with zeros
-test_prepared = pd.read_csv(root_folder+'data/predict_here/data_reduced49.csv')
+# TEST 1: used trained model to compute predictions on the test data
+test_prepared = pd.read_csv(root_folder+'data/test/data_reduced49.csv')
 predictors_test = test_prepared.iloc[:, :-1]
 labels_test = np.array(test_prepared.iloc[:, -1:]).flatten()
 yhat = model.predict(predictors_test)
@@ -43,10 +39,3 @@ pd_result['observations'] = labels_test
 pd_result['predictions'] = yhat
 pd_result.to_csv(root_folder+'data/predictions/test1.csv', index=False)
 
-# TEST 2: compute predictions from a 'features' file computed using the GEE script available in this repository
-features_prepared = pd.read_csv(root_folder+'data/predict_here/features_prepared.csv')
-test_prepared = fillEmptyColumns(model.feature_names_in_, features_prepared)
-test_prepared = test_prepared[model.feature_names_in_]
-yhat = model.predict(test_prepared)
-pd_result =  pd.DataFrame({'predictions':yhat})
-pd_result.to_csv(root_folder+'data/predictions/test2.csv', index=False)
